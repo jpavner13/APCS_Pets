a4355319a54ecf6c91196ba855b33ad9504f8881
Matthew Cadol <cadolmb@s.dcsdk12.org>
2020-01-27 07:55:14 -0700

    test

--- Test.java
+++ Test.java
@@ -3,15 +3,15 @@ public class Test{
         Pet dog1 = new Dog("Hank");
         Pet cat1 = new Cat("Forrest");
         PetOwner bill = new PetOwner(dog1, cat1);
-
+        
         dog1.feedPet();
         cat1.feedPet();
 
         dog1.walkPet();
         cat1.walkPet();
 
-        dog1.petPet(); 
-        cat1.petPet(); 
+        dog1.petPet();
+        cat1.petPet();
 
         dog1.speak();
         cat1.speak();
